2b21df10bdd67042f3080c26f90b001ceb8009b9
Kashif Ahmad <kashifuop99@gmail.com>
2017-04-06 14:02:01 +0500

    commit

--- app/src/main/java/com/ghosttech/kptrafficapp/utilities/Configuration.java
+++ app/src/main/java/com/ghosttech/kptrafficapp/utilities/Configuration.java
@@ -11,7 +11,7 @@ import com.ghosttech.kptrafficapp.R;
  */
 
 public class Configuration {
-    public static final String END_POINT_LIVE = "http://103.240.220.76/kptraffic/kp-traffic-police/";
+    public static final String END_POINT_LIVE = "http://103.240.220.76/kptraffic/";
     public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
     public static final String FILE_UPLOAD_URL = "http://swatshawls.com/lostfound/LostFounds/listupload?";
     public static final String COMPLAINT_MODULE_URL = "http://103.240.220.76/kptraffic/complaints/image";
