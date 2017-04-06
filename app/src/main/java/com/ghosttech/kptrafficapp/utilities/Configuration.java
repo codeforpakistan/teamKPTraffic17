ba80f6a8d0b1ad0243f7c15268329102ec41f524
zahid--ali <zahid.khan@sdvision.net>
2017-04-06 11:07:43 +0300

    registeration type request set

--- app/src/main/java/com/ghosttech/kptrafficapp/utilities/Configuration.java
+++ app/src/main/java/com/ghosttech/kptrafficapp/utilities/Configuration.java
@@ -1,17 +1,11 @@
 package com.ghosttech.kptrafficapp.utilities;
 
-import android.content.Context;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-
-import com.ghosttech.kptrafficapp.R;
-
 /**
  * Created by Asus on 3/29/2017.
  */
 
 public class Configuration {
-    public static final String END_POINT_LIVE = "http://103.240.220.76/kptraffic/kp-traffic-police/";
+    public static final String END_POINT_LIVE = "http://kpktraffic.techeasesol.com/";
     public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
     public static final String FILE_UPLOAD_URL = "http://swatshawls.com/lostfound/LostFounds/listupload?";
     public static final String COMPLAINT_MODULE_URL = "http://103.240.220.76/kptraffic/complaints/image";
