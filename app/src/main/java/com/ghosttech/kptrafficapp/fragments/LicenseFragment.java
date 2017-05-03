db7ba7d311d8da9e534f6f6e1969b6b48720be6d
Kashif Ahmad <kashifuop99@gmail.com>
2017-05-03 16:15:29 +0500

    update screen done, 03/05/2017

--- app/src/main/java/com/ghosttech/kptrafficapp/fragments/LicenseFragment.java
+++ app/src/main/java/com/ghosttech/kptrafficapp/fragments/LicenseFragment.java
@@ -1,20 +1,14 @@
 package com.ghosttech.kptrafficapp.fragments;
 
-import android.app.Dialog;
 import android.app.Fragment;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatEditText;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -27,7 +21,6 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 public class LicenseFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
