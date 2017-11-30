dcc09ee46d884a82d8553a7480b07e76081cfa0f
Kashif Ahmad <kashifuop99@gmail.com>
2017-12-01 01:16:39 +0500

    Education list changed

--- teamTrafficAndroidApp/app/src/main/java/com/kptrafficpolice/trafficapp/fragments/TrafficEducationFragment.java
+++ teamTrafficAndroidApp/app/src/main/java/com/kptrafficpolice/trafficapp/fragments/TrafficEducationFragment.java
@@ -1,14 +1,10 @@
 package com.kptrafficpolice.trafficapp.fragments;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -17,16 +13,16 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.kptrafficpolice.trafficapp.R;
@@ -41,40 +37,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 import static com.thefinestartist.utils.service.ServiceUtil.getSystemService;
-//raabta
-//rabta
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TrafficEducationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TrafficEducationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TrafficEducationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
     private MaterialSearchBar searchBar;
-    List<TrafficEducationHelper> data = new ArrayList<>();
+    List<TrafficEducationHelper> ListTrafficEducation;
     View view;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     TextView tvNoDataFound;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private OnFragmentInteractionListener mListener;
+    private GridView mGridView;
+    private TrafficEducationAdapter mAdapter;
     SweetAlertDialog pDialog;
     TrafficEducationHelper trafficEducationHelper;
     private FirebaseAnalytics mFirebaseAnalytics;
@@ -83,20 +62,12 @@ public class TrafficEducationFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TrafficEducationFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static TrafficEducationFragment newInstance(String param1, String param2) {
         TrafficEducationFragment fragment = new TrafficEducationFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -104,10 +75,7 @@ public class TrafficEducationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -122,7 +90,7 @@ public class TrafficEducationFragment extends Fragment {
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "6");
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Traffic Education Clicked");
         mFirebaseAnalytics.logEvent("Traffic_Education_List", bundle);
-        tvNoDataFound = (TextView)view.findViewById(R.id.tv_no_data_found);
+        tvNoDataFound = (TextView) view.findViewById(R.id.tv_no_data_found);
         tvNoDataFound.setVisibility(View.GONE);
 
         customActionBar();
@@ -130,9 +98,9 @@ public class TrafficEducationFragment extends Fragment {
         searchBar = (MaterialSearchBar) view.findViewById(R.id.searchBar);
         searchBar.setHint("Search");
         searchBar.setSpeechMode(false);
-        data.clear();
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.rv);
-        mRecyclerView.setOnTouchListener(new View.OnTouchListener() {
+        // ListTrafficEducation.clear();
+        mGridView = (GridView) view.findViewById(R.id.gridView);
+        mGridView.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -143,176 +111,65 @@ public class TrafficEducationFragment extends Fragment {
                 return false;
             }
         });
-//        final SwipeRefreshLayout swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
-//        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                // Your code to refresh the list here.
-//                // Make sure you call swipeContainer.setRefreshing(false)
-//                // once the network request has completed successfully.
-//                data.clear();
-//                apiCall();
-//                swipeContainer.setRefreshing(false);
-//
-//            }
-//        });
-//        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-//                android.R.color.holo_green_light,
-//                android.R.color.holo_orange_light,
-//                android.R.color.holo_red_light);
-
         pDialog = new SweetAlertDialog(getActivity(), SweetAlertDialog.PROGRESS_TYPE);
         pDialog.getProgressHelper().setBarColor(Color.parseColor("#179e99"));
         pDialog.setTitleText("Loading data");
+        //apiCall();
+        ListTrafficEducation = new ArrayList<>();
+        mAdapter = new TrafficEducationAdapter(getActivity(), R.layout.traffic_education_items, ListTrafficEducation);
+        mGridView.setAdapter(mAdapter);
         apiCall();
         searchEducationList();
         return view;
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
     public void apiCall() {
         pDialog.show();
         String url = Configuration.END_POINT_LIVE + "traffic_education/getEducationSigns";
-        final Bundle args = new Bundle();
-//
-//        Log.d("zma url", url);
-        StringRequest jsonObjRequest = new StringRequest(Request.Method.POST,
-                url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            pDialog.dismiss();
-                            showJSON(response);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            pDialog.dismiss();
-                            new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
-                                    .setTitleText("Server Error")
-                                    .setConfirmText("OK")
-                                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                                        @Override
-                                        public void onClick(SweetAlertDialog sweetAlertDialog) {
-                                            Fragment fragment = new MainFragment();
-                                            getFragmentManager().beginTransaction().
-                                                    replace(R.id.fragment_container, fragment).commit();
-                                        }
-                                    })
-                                    .show();
+                    JSONArray data = response.getJSONArray("data");
+                    for (int i = 0; i < data.length(); i++) {
+                        pDialog.dismiss();
+                        Log.d("zma traf respo", String.valueOf(response));
+                        JSONObject jsonObject = data.getJSONObject(i);
+                        trafficEducationHelper = new TrafficEducationHelper();
+                        trafficEducationHelper.strImageTitle = jsonObject.getString("image_title");
+                        trafficEducationHelper.strDescriptionEnglish = jsonObject.getString("image_description_eng");
+                        trafficEducationHelper.strDescriptionUrdu = jsonObject.getString("image_description_urdu");
+                        trafficEducationHelper.strImage = jsonObject.getString("image");
+                        if (jsonObject.getString("image").contains(".gif")) {
+                            Log.d("zma image", jsonObject.getString("image"));
+                            Configuration.Traffic_Education_Gif_Image_Boolean = true;
+                            trafficEducationHelper.strImage = jsonObject.getString("image");
+
                         }
+                        if (data.length() == 0) {
+                            pDialog.dismiss();
+                            tvNoDataFound.setVisibility(View.VISIBLE);
+                        }
+                        ListTrafficEducation.add(trafficEducationHelper);
                     }
-                }, new Response.ErrorListener() {
-
+                    mAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d("zma exception edu", String.valueOf(e));
+                }
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                pDialog.dismiss();
-                new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
-                        .setTitleText("Server Error")
-                        .setConfirmText("OK")
-                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                            @Override
-                            public void onClick(SweetAlertDialog sweetAlertDialog) {
-                                sweetAlertDialog.dismiss();
-                                Fragment fragment = new MainFragment();
-                                getFragmentManager().beginTransaction().
-                                        replace(R.id.fragment_container, fragment).commit();
 
-                            }
-                        })
-                        .show();
             }
-        }) {
-            @Override
-            public String getBodyContentType() {
-                return "application/x-www-form-urlencoded;charset=UTF-8";
-            }
-
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-
-                //params.put("dl_license",strLicenseNumber);
-                Log.d("zma params", String.valueOf(params));
-                return params;
-            }
-
-        };
-        jsonObjRequest.setRetryPolicy(new DefaultRetryPolicy(20000,
+        });
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(200000,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-        requestQueue.add(jsonObjRequest);
-    }
-
-    private void showJSON(String response) throws JSONException {
-        JSONObject json = new JSONObject(response);
-
-        if (json.has("data")) {
-            JSONArray jsonArray = json.getJSONArray("data");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                trafficEducationHelper = new TrafficEducationHelper();
-                trafficEducationHelper.strImageTitle = jsonObject.getString("image_title");
-                trafficEducationHelper.strDescriptionEnglish = jsonObject.getString("image_description_eng");
-                trafficEducationHelper.strDescriptionUrdu = jsonObject.getString("image_description_urdu");
-                trafficEducationHelper.strImage = jsonObject.getString("image");
-                if (jsonObject.getString("image").contains(".gif")) {
-                    Log.d("zma image", jsonObject.getString("image"));
-                    Configuration.Traffic_Education_Gif_Image_Boolean = true;
-                    trafficEducationHelper.strImage = jsonObject.getString("image");
-
-                }
-                if (jsonArray.length() == 0) {
-                    pDialog.dismiss();
-                    tvNoDataFound.setVisibility(View.VISIBLE);
-                }
-
-                data.add(trafficEducationHelper);
-            }
-
-
-            mLayoutManager = new LinearLayoutManager(getActivity());
-            mRecyclerView.setLayoutManager(mLayoutManager);
-
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            mRecyclerView.setHasFixedSize(true);
-
-            // specify an adapter (see also next example)
-            mAdapter = new TrafficEducationAdapter(getActivity(), data);
-            mRecyclerView.setAdapter(mAdapter);
-        }
-
-        mAdapter.notifyDataSetChanged();
-
+        requestQueue.add(jsonObjectRequest);
     }
 
     public void customActionBar() {
@@ -324,14 +181,6 @@ public class TrafficEducationFragment extends Fragment {
         TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
         ImageView mBackArrow = (ImageView) mCustomView.findViewById(R.id.iv_back_arrow);
         mTitleTextView.setText("Traffic Education");
-//        mBackArrow.setImageResource(R.drawable.back_arrow);
-//        mBackArrow.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Fragment fragment = new MainFragment();
-//                getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-//            }
-//        });
         mActionBar.setCustomView(mCustomView);
         mActionBar.setDisplayShowCustomEnabled(true);
     }
@@ -351,20 +200,18 @@ public class TrafficEducationFragment extends Fragment {
                 // Toast.makeText(TrafficSigns.this, "Query is: "+query, Toast.LENGTH_SHORT).show();
                 List<TrafficEducationHelper> newData = new ArrayList<>();
 
-                for (int j = 0; j < data.size(); j++) {
-                    final String test = data.get(j).strImageTitle.toLowerCase();
-                    final String test2 = data.get(j).strDescriptionEnglish.toLowerCase();
+                for (int j = 0; j < ListTrafficEducation.size(); j++) {
+                    final String test = ListTrafficEducation.get(j).strImageTitle.toLowerCase();
+                    final String test2 = ListTrafficEducation.get(j).strDescriptionEnglish.toLowerCase();
 
-                 /*  final String text = trafficEducationHelper.strImageTitle.toLowerCase();
-                    final String text2 = trafficEducationHelper.strDescriptionEnglish.toLowerCase();
-                    Log.e("matched", text+"\n");*/
+
                     if (test.contains(query) || test2.contains(query)) {
-                        newData.add(data.get(j));
+                        newData.add(ListTrafficEducation.get(j));
                     }
                 }
                 // specify an adapter (see also next example)
-                mAdapter = new TrafficEducationAdapter(getActivity(), newData);
-                mRecyclerView.setAdapter(mAdapter);
+                 mAdapter = new TrafficEducationAdapter(getActivity(),  R.layout.traffic_education_items, newData);
+                mGridView.setAdapter(mAdapter);
                 mAdapter.notifyDataSetChanged();
             }
 
--- teamTrafficAndroidApp/app/src/main/java/com/kptrafficpolice/trafficapp/utilities/TrafficEducationAdapter.java
+++ teamTrafficAndroidApp/app/src/main/java/com/kptrafficpolice/trafficapp/utilities/TrafficEducationAdapter.java
@@ -8,6 +8,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -17,6 +18,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +26,7 @@ import com.bumptech.glide.Glide;
 import com.kptrafficpolice.trafficapp.R;
 import com.kptrafficpolice.trafficapp.fragments.TrafficEducationDetailFragment;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -32,96 +35,48 @@ import uk.co.senab.photoview.PhotoViewAttacher;
 /**
  * Created by Fazal Mola on 5/15/2017.
  */
-public class TrafficEducationAdapter extends RecyclerView.Adapter<TrafficEducationAdapter.ViewHolder> {
+public class TrafficEducationAdapter extends ArrayAdapter{
+    private Context context;
+    List<TrafficEducationHelper> data = new ArrayList<>();
+    private int layoutResourceId;
 
-    List<TrafficEducationHelper> data = Collections.emptyList();
-    Context context;
-    int loader;
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        CardView cvTrafficEducation;
-        TextView descriptionEnglish, descriptionUrdu, imageTitle;
-        ImageView ivTrafficEducationImage;
-
-
-        public ViewHolder(View v) {
-            super(v);
-            cvTrafficEducation = (CardView) itemView.findViewById(R.id.cv_traffic_education);
-            imageTitle = (TextView) itemView.findViewById(R.id.tv_image_title);
-            descriptionEnglish = (TextView) itemView.findViewById(R.id.description_english);
-            descriptionUrdu = (TextView) itemView.findViewById(R.id.description_urdu);
-            ivTrafficEducationImage = (ImageView) itemView.findViewById(R.id.traffic_education_image);
-
-        }
-    }
-
-
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public TrafficEducationAdapter(Context context, List<TrafficEducationHelper> data) {
+    public TrafficEducationAdapter(Context context, int layoutResourceId, List<TrafficEducationHelper> data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
         this.context = context;
         this.data = data;
-        ;
-    }
-    //raabta
-//rabta
-    // Create new views (invoked by the layout manager)
-    @Override
-    public TrafficEducationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                                 int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.traffic_education_items, parent, false);
-
-        // set the view's size, margins, paddings and layout parameters
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        ViewHolder holder;
 
-        Log.d("position", String.valueOf(position));
-        final TrafficEducationHelper educationHelper = data.get(position);
-        String descUrdu = String.valueOf(educationHelper.strDescriptionUrdu);
-        SpannableString content = new SpannableString("Login");
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        // tvSkip.setText(content);
-//        descUrdu =descUrdu.substring(0,10);
-        holder.imageTitle.setText(educationHelper.strImageTitle);
-        holder.descriptionEnglish.setText(educationHelper.strDescriptionEnglish);
-        holder.descriptionUrdu.setText(educationHelper.strDescriptionUrdu);
-        Glide.with(context).load(Configuration.END_POINT_LIVE+"uploads/traffic-education/" + educationHelper.strImage)
-                .into(holder.ivTrafficEducationImage);
+        if (row == null) {
+            LayoutInflater inflater = ((AppCompatActivity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);            holder = new ViewHolder();
+            holder.imageTitle = (TextView) row.findViewById(R.id.tv_image_title);
+            holder.image = (ImageView) row.findViewById(R.id.traffic_education_image);
+            row.setTag(holder);
+        } else {
+            holder = (ViewHolder) row.getTag();
+        }
 
-        holder.cvTrafficEducation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment fragment = new TrafficEducationDetailFragment();
-                Bundle args = new Bundle();
-                args.putString("title", String.valueOf(educationHelper.strImageTitle));
-                args.putString("englishDescription", String.valueOf(educationHelper.strDescriptionEnglish));
-                args.putString("urduDescription", String.valueOf(educationHelper.strDescriptionUrdu));
-                args.putString("image", String.valueOf(educationHelper.strImage));
-                fragment.setArguments(args);
-                ((AppCompatActivity) context).getFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, fragment).addToBackStack("tag").commit();
-
-            }
-        });
-
+        TrafficEducationHelper item = data.get(position);
+        holder.imageTitle.setText(item.strImageTitle);
+       // Log.d("zma image title", item.strImageTitle);
+        Glide.with(context).load(Configuration.END_POINT_LIVE+"uploads/traffic-education/" + item.strImage)
+                .into(holder.image);
+        return row;
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
+    static class ViewHolder {
+        TextView imageTitle;
+        ImageView image;
+    }
+
     @Override
-    public int getItemCount() {
+    public int getCount() {
         return data.size();
-    }
-
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
     }
 }
